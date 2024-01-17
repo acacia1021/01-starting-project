ceab173defa9e4e9c6c875e605dce7bab4d6cc93
AcaciaVA <acacia.lord@va.gov>
2024-01-17 11:30:43 -0500

    38. Creating and Using a First Custom Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-// Is made of JavaScript Syntax Extension, which is used to describe & 
+// Is made of JavaScript Syntax Extension, which is used to describe &
 // create HTML elements in JavaScript in a declarative way
 
-// REACT projects come with a build process that transforms JSX code 
+// REACT projects come with a build process that transforms JSX code
 // (behind the scenes) to code that does work in browsers
 
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
 
 function App() {
   return (
     <div>
-      <header>
+      <Header></Header>
+      {/* <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           Fundamental React concepts you will need for almost any app you are
           going to build!
         </p>
-      </header>
+      </header> */}
       <main>
         <h2>Time to get started!</h2>
       </main>
