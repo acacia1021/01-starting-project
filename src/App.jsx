78edad6e069c375fb99bc2f804fff83af19f7a7b
AcaciaVA <acacia.lord@va.gov>
2024-01-17 09:31:57 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// Is made of JavaScript Syntax Extension, which is used to describe & 
+// create HTML elements in JavaScript in a declarative way
+
+// REACT projects come with a build process that transforms JSX code 
+// (behind the scenes) to code that does work in browsers
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
